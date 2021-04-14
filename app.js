e8214dbc1698ffd60549d2b1bda8504448350fa5
mohamedelnadry <mohamdelnadry12345@gmail.com>
2021-04-14 16:20:48 +0200

    first commit

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.set('view engine', 'ejs')
 app.use(require('./routes/Register.routes'))
 app.use(require('./routes/Login.routes'))
 app.use(require('./routes/Home.routes'))
+app.get('*',(req,res)=>{
+  res.send("404 page not found")
+})
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.fjtlc.mongodb.net/test_db', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('connected');
